d5bef2b9de6644c60e96697cff058b95d075890a
Weston Wood <stakin101@gmail.com>
2026-03-31 10:18:46 -0700

    testing desktop app wrapping

--- orion-browser.nix
+++ orion-browser.nix
@@ -1,7 +1,10 @@
 {
   lib,
   flatpak,
+  symlinkJoin,
   writeShellScriptBin,
+  makeDesktopItem,
+  runCommand,
   fetchurl,
   url,
   hash,
@@ -17,14 +20,43 @@ let
     inherit url hash;
     name = "orion-${version}.flatpak";
   };
+
+  iconFile = fetchurl {
+    url = "https://browser.kagi.com/press-kit/icon-main-logo.png";
+    hash = "sha256-dINt5zkw2kCR7U3GvCGV0EBtqLWC7vg3H3LPeCbpwKI=";
+  };
+  iconDrv = runCommand "${pname}-icon" { } ''
+    mkdir -p $out/share/icons/hicolor/256x256/apps
+    cp ${iconFile} $out/share/icons/hicolor/256x256/apps/${pname}.png
+  '';
+
+  binScript = writeShellScriptBin pname ''
+    export PATH="${lib.makeBinPath [ flatpak ]}:$PATH"
+    flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    if ! flatpak info --user ${appId} >/dev/null 2>&1; then
+      echo "Installing Orion Flatpak bundle..."
+      flatpak install --user --noninteractive --assumeyes ${flatpakBundle}
+    fi
+    export XDG_DATA_DIRS="$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
+    exec flatpak run --user ${appId} "$@"
+  '';
+
+  desktopItem = makeDesktopItem {
+    name = pname;
+    desktopName = "Orion Browser";
+    exec = "${binScript}/bin/${pname}";
+    icon = pname; 
+    categories = [
+      "Network"
+      "WebBrowser"
+    ];
+  };
 in
-writeShellScriptBin pname ''
-  export PATH="${lib.makeBinPath [ flatpak ]}:$PATH"
-  flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-  if ! flatpak info --user ${appId} >/dev/null 2>&1; then
-    flatpak update --user --noninteractive
-    flatpak install --user --noninteractive --assumeyes ${flatpakBundle}
-  fi
-  export XDG_DATA_DIRS="$HOME/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
-  exec flatpak run --user ${appId} "$@"
-''
+symlinkJoin {
+  name = pname;
+  paths = [
+    binScript
+    desktopItem
+    iconDrv
+  ];
+}
